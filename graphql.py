b2adc71e26239b4a5232228c7a28a266bde58cfe
cooperyoung <coopergyoung@gmail.com>
2023-04-22 17:11:33 -0700

    F - created Field class with appropriate attributes

--- graphql.py
+++ graphql.py
@@ -1,6 +1,22 @@
 import requests
 import json
 
+class Field:
+    def __init__(self, Name):
+        self.kind   #Str         # kimd of Field            ex: SCALAR  or  OBJECT  or  NON_NULL
+        self.name = Name         # String name of Field            ex: User
+        self.args = []           # list of arguments        ex: User (id: xx)    id is an args
+        self.description  #Str   # description of Field      
+        self.subfields = []      #
+        self.isDeprecated        # Boolean
+        self.deprecationReason = "null"         # change if isDeprecated == True
+        self.type = {
+            "kind",
+            "name",
+            "ofType"
+        }                       # dictionary used to classfy subiems
+        self.locations = []     # not sure what this is used for (array of strings)
+
 url = "https://graphql-catalog.app.iherb.com/"
 
 query = '''
@@ -31,9 +47,3 @@ response_json = json.loads(response_string)
 error_message = response_json["errors"][0]["message"]
 suggestion = error_message.split("Did you mean ")[1].strip(" '?")
 print(suggestion)
-
-
-
-
-
-
