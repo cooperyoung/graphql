11de2f32ded8549a7dbc421532501803ccb3617b
cooperyoung <coopergyoung@gmail.com>
2023-04-26 15:25:11 -0700

    F!! wrote pseudocode for obtaining original list of Fields

--- graphql.py
+++ graphql.py
@@ -49,7 +49,37 @@ headers = {
 response = requests.post(url, data=json.dumps(payload), headers=headers)
 print(response.json())
 
-# Extract the suggestion from the error message
-# error_message = response_json["errors"][0]["message"]
-# suggestion = error_message.split("Did you mean ")[1].strip(" '?")
-# print(suggestion)
+# Examples of Error Messages:
+    # 'message': 'Cannot query field "account" on type "Query".'
+    # 'message': 'Cannot query field "pser" on type "Query". Did you mean "user"?'
+    # 'message': 'Field "user" argument "id" of type "String!" is required, but it was not provided.'
+    # 'message': 'Field "user" of type "User" must have a selection of subfields. Did you mean "user { ... }"?'
+
+
+# How to extract the suggestion from the error message: 
+    # error_message = response_json["errors"][0]["message"]
+    # suggestion = error_message.split("Did you mean ")[1].strip(" '?")
+    # print(suggestion)
+
+
+
+# Psudo code:
+
+# load a .txt file of words and call it word_list
+
+i = 0                                       # set a counter to 0
+for error_message in response:              # range through the 'messages' in response (range n in response_json["errors"][n]["message"])
+    current_guess = word_list[i]            # update the current guessed word
+    current_error_message = ErrorMessage()  # initialize ErrorMessage class
+    current_error_message.process()         # call a function to populate ErrorMessage class variabes
+                                                    # ex: real_word = error_message.split("Did you mean ")[1].strip(" '?")
+    while(current_guess != current_error_message.guessed_word):  # while the error message word isn't the 
+        new_field = Field(word_list[i])     # create a Field from the current_guess (no error means its a field)
+        field_list.insert(new_field)        # add new field to list
+        i += 1                              # increase counter by 1
+
+    # by now the current_guess should == current_error_message.guessed_word
+
+    if (current_error_message.real_word is not None):    # if the error message reveals a real word
+        new_field = current_error_message.guessed_word   # make a new field and add relevent info (args/subfields/etc)
+    
